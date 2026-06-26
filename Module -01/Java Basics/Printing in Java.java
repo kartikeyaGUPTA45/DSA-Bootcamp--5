397e89dceb036a4c17ac615fe88889a82080fc65
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-06-26 22:01:16 +0530

    Create Printing in Java.java

---
+++ Module -01/Java Basics/Printing in Java.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    System.out.print("Welcome to the course");
+	    System.out.print("Java Basics");
+	    System.out.print("Varaibles");
+	    
+	    System.out.print("Welcome to the course \n");
+	    System.out.print("Java Basics \n");
+	    System.out.print("Varaibles \n");
+	    
+	    System.out.println("Welcome to the course");
+	    System.out.println("Java Basics");
+	    System.out.println("Varaibles");
+	}
+}
