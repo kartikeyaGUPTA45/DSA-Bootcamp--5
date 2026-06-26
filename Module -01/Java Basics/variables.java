d01246d105ddbc61e48b7c70cdb86e4cf91754d8
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-06-26 22:32:08 +0530

    Create variables.java

---
+++ Module -01/Java Basics/variables.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int a = 50;
+	    long b = 60;
+	    double c = 79.34;
+	    char ch = 'a';
+	    String str = "asjfbsdjkgbskdj";
+	    boolean flag = true;
+	    
+	    System.out.println(a);
+	    System.out.println(b);
+	    System.out.println(c);
+	    System.out.println(ch);
+	    System.out.println(str);
+	    System.out.println(flag);
+	   
+	}
+}
