66db2484eb48a9a438225ade3d2d83fee1aadaf2
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-06-27 22:06:04 +0530

    Create Assignment Operators.java

---
+++ Module -01/Java Basics/Assignment Operators.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int a = 30;
+		System.out.println(a);
+		
+		a+=20;
+		
+		System.out.println(a);
+		
+		a-=10;
+		System.out.println(a);
+		
+		a*=15;
+		System.out.println(a);
+		
+		a/=20;
+		System.out.println(a);
+		
+	}
+}
