23214c1e9635b992eeda7bca3dc31a29041e0993
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-06-27 21:34:09 +0530

    Create Arithmetic Operators.java

---
+++ Module -01/Java Basics/Arithmetic Operators.java
@@ -0,0 +1,34 @@
+public class Main
+{
+	public static void main(String[] args) {
+// 		int a = 3;
+// 		int b = 2;
+		
+// 		int ans1 = a+b;
+// 		int ans2 = a-b;
+// 		int ans3 = a*b;
+// 		int ans4 = a/b;
+// 		int ans5 = a%b;
+		
+// 		System.out.println("Add a and b: " + ans1);
+// 		System.out.println("Subtract a and b: " + ans2);
+// 		System.out.println("Multiply a and b: " + ans3);
+// 		System.out.println("Divide a and b: " + ans4);
+// 		System.out.println("Remainder a and b: " + ans5);
+
+        double a = 3;
+		double b = 2;
+		
+		double ans1 = a+b;
+		double ans2 = a-b;
+		double ans3 = a*b;
+		double ans4 = a/b;
+		double ans5 = a%b;
+		
+		System.out.println("Add a and b: " + ans1);
+		System.out.println("Subtract a and b: " + ans2);
+		System.out.println("Multiply a and b: " + ans3);
+		System.out.println("Divide a and b: " + ans4);
+		System.out.println("Remainder a and b: " + ans5);
+	}
+}
