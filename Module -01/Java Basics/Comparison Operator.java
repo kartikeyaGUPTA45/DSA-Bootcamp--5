89bced669ca9cf1f1d93660200a635c562dda104
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-06-27 22:20:56 +0530

    Create Comparison Operator.java

---
+++ Module -01/Java Basics/Comparison Operator.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int a = 30, b = 30;
+		
+		System.out.println(a == b); // 20 == 30 -> false
+		System.out.println(a != b); // 20 != 30 -> true
+		System.out.println(a > b); // 20 > 30 -> false
+		System.out.println(a >= b); // 20 >= 30 -> false || 30 >= 30 true
+		System.out.println(a < b); // 20 < 30 -> true
+		System.out.println(a <= b); // 20 <= 30 -> true  | 30 <= 30 true
+		
+		
+	}
+}
