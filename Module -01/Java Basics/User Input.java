a9cc2775a61c4c181000ff1756859fc01842aa9c
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-03 21:36:03 +0530

    Create User Input.java

---
+++ Module -01/Java Basics/User Input.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the input for a");
+		int a = scn.nextInt();
+		System.out.println("Enter the input for b");
+		long b = scn.nextLong();
+		System.out.println("Enter the input for c");
+		boolean c = scn.nextBoolean();
+		
+		
+		System.out.println("User input for a is : " + a);
+		System.out.println("User input for b is : " + b);
+		System.out.println("User input for c is : " + c);
+	}
+}
