f1844e6378bead2d3853c89f0cf8600bb4de09bd
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-05 10:56:42 +0530

    Create for loop.java

---
+++ Module -01/Java Basics/for loop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scn = new Scanner(System.in);
+		
+// 		for(int i=1;i<=10;i++) {
+// 		    System.out.println("*****");
+// 		}
+
+        for(int i=10;i>=1;i--) {
+            System.out.println("*****");
+        }
+		
+	}
+}
