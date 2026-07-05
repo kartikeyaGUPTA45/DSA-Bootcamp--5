40eae08233086f858c4dd8fcc13e4e770bdc0159
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-05 11:11:36 +0530

    Create while loop.java

---
+++ Module -01/Java Basics/while loop.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scn = new Scanner(System.in);
+		
+        // int i=1;
+        // while(i<=10) {
+        //     System.out.println("*****");
+        //     // i+=1;
+        //     i++;
+        // }
+        
+        int i=10;
+        while(i>=1) {
+            System.out.println("*****");
+            i-=1;
+        }
+        
+		
+	}
+}
