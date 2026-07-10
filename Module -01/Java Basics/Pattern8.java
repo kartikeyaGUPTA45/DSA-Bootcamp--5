64a89995c207c590222745e96560ae622fc4827f
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-10 23:00:18 +0530

    Add Pattern8.java for Fibonacci pattern printing

---
+++ Module -01/Java Basics/Pattern8.java
@@ -0,0 +1,30 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n = 5;
+		int nsp = n/2;
+		int nst = 1;
+		int a = 0, b=1;
+		for(int i=1;i<=n;i++) {
+		    for(int j=1;j<=nsp;j++) {
+		        System.out.print("  ");
+		    }
+		    
+		    for(int j=1;j<=nst;j++) {
+		        int c = a+b;
+		        System.out.print(c+" ");
+		        a=b;
+		        b=c;
+		    }
+		    
+		    if (i <= n/2) {
+		        nsp-=1;
+		        nst+=2;
+		    } else {
+		        nsp+=1;
+		        nst-=2;
+		    }
+		    System.out.println();
+		}
+	}
+}
