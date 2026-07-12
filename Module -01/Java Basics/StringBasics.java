5bdfc8776d1b344262a469f87f9440fd80ba4fe0
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-12 10:40:44 +0530

    Create StringBasics.java

---
+++ Module -01/Java Basics/StringBasics.java
@@ -0,0 +1,11 @@
+public class Main {   
+	public static void main(String[] args) {
+	   String str = "abcd efgh ijkl";
+	   
+	   System.out.println(str.length());
+	   System.out.println(str.charAt(7));
+	   
+	   
+	}
+	
+}
