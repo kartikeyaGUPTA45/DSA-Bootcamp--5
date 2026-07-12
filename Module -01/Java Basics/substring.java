f0132f941689d0f2edf2b22426687c280893ff90
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-12 11:42:32 +0530

    Create substring.java

---
+++ Module -01/Java Basics/substring.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {   
+	public static void main(String[] args) {
+	   String str = "abcdefghijkl";
+	   
+	   //System.out.println(str1.substring(1,5)); // 1,2,3,4
+	   //System.out.println(str1.substring(1,6)); // 1,2,3,4,5
+	   
+	   //System.out.println(str1.substring(1));
+	   //System.out.println(str.substring(5,13));
+	   //System.out.println(str.substring(5,12));
+	   //System.out.println(str.substring(5,5));
+	   System.out.println(str.substring(11,5)); // I will check 
+	   //System.out.println(str.substring(14,14));
+	   //System.out.println(str.substring(12)); // Length of the string is 12
+	   //System.out.println(str.substring(13));
+	   
+	}
+}
