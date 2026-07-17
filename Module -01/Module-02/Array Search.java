cc3e6b13acdd8ef59dc86f35f3727c1c08e3d27c
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-17 22:16:14 +0530

    Create Array Search.java

---
+++ Module -01/Module-02/Array Search.java
@@ -0,0 +1,18 @@
+//Problem Link: https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1#
+
+Solution: 
+
+class Solution {
+    public int search(int arr[], int x) {
+        int n = arr.length;
+        
+        for(int i=0;i<n;i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        
+        return -1;
+        
+    }
+}
