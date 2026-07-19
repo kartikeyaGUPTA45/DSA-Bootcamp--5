c1f8ecce368e7a84343327729635a2393caebae0
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-19 15:58:17 +0530

    Create 566. Reshape the Matrix.java

---
+++ Module -02/566. Reshape the Matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int totalElements = m*n;
+
+        if (totalElements != (r*c)) {
+            return mat;
+        }
+
+        int res[][] = new int[r][c];
+
+        for(int i=0;i<totalElements;i++) {
+            res[i/c][i%c] = mat[i/n][i%n];
+        }
+
+        return res;
+    }
+}
