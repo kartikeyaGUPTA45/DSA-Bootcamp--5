87dc9adcba5e94b906d8cfea8f1cbfa6ab7d897c
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-26 11:27:18 +0530

    Update 1832. Check if the Sentence Is Pangram.java

--- Module -02/String Practice/1832. Check if the Sentence Is Pangram.java
+++ Module -02/String Practice/1832. Check if the Sentence Is Pangram.java
@@ -1,1 +1,22 @@
 //Problem Link: https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+//Solution:
+
+class Solution {
+    public boolean checkIfPangram(String s) {
+        int freq[] = new int [26];
+
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            int idx = ch-'a';
+            freq[idx]+=1;
+        }
+
+        for(int i=0;i<26;i++) {
+            if (freq[i] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
