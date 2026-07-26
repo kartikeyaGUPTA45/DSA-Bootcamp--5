bd542cd96f457d96721daedae16822d1de69c3e0
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-26 11:20:49 +0530

    Update Check for Isogram String.java

--- Module -02/String Practice/Check for Isogram String.java
+++ Module -02/String Practice/Check for Isogram String.java
@@ -1,1 +1,29 @@
 //Problem Link: https://www.geeksforgeeks.org/problems/check-if-a-string-is-isogram-or-not-1587115620/1
+
+//Solution:
+
+
+
+class Solution {
+    // Function to check if a string is Isogram or not.
+    static boolean isIsogram(String data) {
+        // Your code here
+        int freq[] = new int[26];
+        
+        for(int i=0;i<data.length();i++) {
+            char ch = data.charAt(i);
+            int idx = ch-'a';
+            
+            freq[idx]+=1;
+        }
+        
+        for(int i=0;i<26;i++) {
+            if (freq[i] > 1) {
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+}
