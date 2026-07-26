d09f1851fd5e169c8d548b3516c48cd53ee61503
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-26 11:44:22 +0530

    Update 242. Valid Anagram.java

--- Module -02/String Practice/242. Valid Anagram.java
+++ Module -02/String Practice/242. Valid Anagram.java
@@ -1,1 +1,35 @@
 //Problem Link: https://leetcode.com/problems/valid-anagram/description/
+
+// Solution: 
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int freq[] = new int[26];
+
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            int idx = ch-'a';
+            freq[idx]+=1;
+        }
+
+
+        for(int i=0;i<t.length();i++) {
+            char ch = t.charAt(i);
+            int idx = ch-'a';
+            freq[idx]-=1;
+        }
+
+        for(int i=0;i<26;i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    } 
+}
