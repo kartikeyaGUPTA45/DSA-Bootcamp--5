feb11d699b72ba4d9442ae3eabf3311b36c1fb6d
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-26 11:36:53 +0530

    Update Toggle Case.java

--- Module -02/String Practice/Toggle Case.java
+++ Module -02/String Practice/Toggle Case.java
@@ -1,1 +1,22 @@
 //Problem Link: https://www.geeksforgeeks.org/problems/toggle-case/1
+
+// Solution:
+
+class Solution {
+    public String toggleCase(String s) {
+        // code here
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            
+            if (ch >= 'a' && ch <= 'z') {
+                sb.append((char)(ch-'a'+'A'));
+            } else {
+                sb.append((char)(ch-'A'+'a'));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
