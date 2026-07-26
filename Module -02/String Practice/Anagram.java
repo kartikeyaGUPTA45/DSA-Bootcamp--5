1ebf7591893af40a9304ec5bdc9fa204204dff45
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-26 11:47:10 +0530

    Update Anagram.java

--- Module -02/String Practice/Anagram.java
+++ Module -02/String Practice/Anagram.java
@@ -1,1 +1,35 @@
 //Problem Link: https://www.geeksforgeeks.org/problems/anagram-1587115620/1
+
+// Solution: 
+
+class Solution {
+    public static boolean areAnagrams(String s, String t) {
+        // code here
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int freq[] = new int[26];
+
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            int idx = ch-'a';
+            freq[idx]+=1;
+        }
+
+
+        for(int i=0;i<t.length();i++) {
+            char ch = t.charAt(i);
+            int idx = ch-'a';
+            freq[idx]-=1;
+        }
+
+        for(int i=0;i<26;i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
