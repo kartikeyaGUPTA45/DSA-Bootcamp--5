6c2017aea49a1e1a1ebcd3b34794de2a3ce7cb9c
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-07-26 11:33:46 +0530

    Update Panagram Checking.java

--- Module -02/String Practice/Panagram Checking.java
+++ Module -02/String Practice/Panagram Checking.java
@@ -1,1 +1,32 @@
 // Problem Link: https://www.geeksforgeeks.org/problems/pangram-checking-1587115620/1
+
+// Solution:
+
+class Solution {
+    public static boolean checkPangram(String s) {
+        // code here
+     
+        int freq[] = new int [26];
+
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch<='z') {
+                int idx = ch-'a';
+                freq[idx]+=1;
+            } else if (ch >= 'A' && ch <='Z') {
+                ch = (char)(ch-'A'+'a');
+                int idx = ch-'a';
+                freq[idx]+=1;
+            }
+        }
+
+        for(int i=0;i<26;i++) {
+            if (freq[i] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+            
