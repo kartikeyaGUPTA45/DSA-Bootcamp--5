ecbe5d355c30b018f26db829434cd17f24ba3de2
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-08-09 11:39:15 +0530

    Create 342. Power of Four.java

---
+++ Module -02/Number Theory/342. Power of Four.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n == 0 || n < 0) {
+            return false;
+        }
+
+        while(n > 1) {
+            int rem = n%4;
+
+            if (rem != 0) {
+                return false;
+            }
+
+            n/=4;
+        }
+
+        return true;
+    }
+}
