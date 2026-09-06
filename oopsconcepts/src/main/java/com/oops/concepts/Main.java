015ab5eda87ef9969f3b011f9d2e2a0909d0e9c8
kartikeyaGUPTA45 <kartikeyagupta1409@gmail.com>
2026-09-06 11:50:58 +0530

    Static

--- oopsconcepts/src/main/java/com/oops/concepts/Main.java
+++ oopsconcepts/src/main/java/com/oops/concepts/Main.java
@@ -7,8 +7,11 @@ public class Main {
 
     // Main is the starting point of code
     public static void main(String[] args) {
-        Log log = new DatabaseLog();
-        Application app = new Application(log);
-        app.run();
+        Student student = new Student();
+        student.name = "John";
+        student.age = "20";
+        student.rollNumber = "1";
+
+        System.out.println(student.name + " " + student.age +  " " + student.rollNumber + " " + Student.school);
     }
 }
---
+++ oopsconcepts/src/main/java/com/oops/concepts/Student.java
@@ -0,0 +1,9 @@
+package com.oops.concepts;
+
+public class Student {
+
+    public String name;
+    public String age;
+    public String rollNumber;
+    public static String school = "ABC public school";
+}
