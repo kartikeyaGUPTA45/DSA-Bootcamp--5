9e962dffeb172e5bfa2304d65443280a487fad64
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2026-09-12 21:26:22 +0530

    Create 278. First Bad Version.java

---
+++ Module -02/Binary Search/278. First Bad Version.java
@@ -0,0 +1,23 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        long low = 1, high = n;
+
+        long ans = 0;
+        while(low <= high) {
+            long mid = (low + high)/2;
+
+            if (isBadVersion((int)mid)) {
+                ans = mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+
+        return (int)ans;
+    }
+}
+
